cb8ff6c46ee16a8597f7ec4551dd255e7c256d8d
NoMeatNo <160690260+NoMeatNo@users.noreply.github.com>
2024-02-25 19:42:31 -0500

    13

--- EinthusanProvider/src/main/kotlin/com/likdev256/EinthusanProvider.kt
+++ EinthusanProvider/src/main/kotlin/com/likdev256/EinthusanProvider.kt
@@ -45,6 +45,8 @@ class EinthusanProvider : MainAPI() { // all providers must be an instance of Ma
     private fun Element.toSearchResult(): SearchResponse? {
         val title = this.selectFirst("div.movie-title h3 a")?.text()?.trim() ?: return null
         val href = this.selectFirst("div.movie-title h3 a")?.attr("href").toString()
+        val posterUrl = this.selectFirst("div.latest-movie-img-container")?.attr("data-src")?.trim()
+
 //        val posterUrl = this.selectFirst("div.latest-movie-img-container")
 //            ?.attr("style")
 //            ?.substringAfter("url(&quot;")
@@ -56,11 +58,11 @@ class EinthusanProvider : MainAPI() { // all providers must be an instance of Ma
 //        val styleAttribute = this.selectFirst("div.latest-movie-img-container")?.attr("style")
 //        val posterUrl = styleAttribute?.substringAfter("url(&quot;")?.substringBefore("&quot;")
 
-        val styleAttribute = this.selectFirst("div.latest-movie-img-container")?.attr("style")
-        val posterUrl = styleAttribute?.let {
-            val regex = Regex("url\\(([^)]+)\\)")
-           regex.find(it)?.groups?.get(1)?.value
-        }
+//        val styleAttribute = this.selectFirst("div.latest-movie-img-container")?.attr("style")
+//        val posterUrl = styleAttribute?.let {
+//            val regex = Regex("url\\(([^)]+)\\)")
+//           regex.find(it)?.groups?.get(1)?.value
+//        }
 
         return newMovieSearchResponse(title, href, TvType.Movie) {
                 this.posterUrl = posterUrl
