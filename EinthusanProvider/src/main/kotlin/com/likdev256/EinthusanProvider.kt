3c2d0e87d87b1524b7b097f7726343c56a646ddb
NoMeatNo <160690260+NoMeatNo@users.noreply.github.com>
2024-02-26 18:33:59 -0500

    Update EinthusanProvider.kt

--- EinthusanProvider/src/main/kotlin/com/likdev256/EinthusanProvider.kt
+++ EinthusanProvider/src/main/kotlin/com/likdev256/EinthusanProvider.kt
@@ -111,7 +111,8 @@ class EinthusanProvider : MainAPI() { // all providers must be an instance of Ma
         // val mp4link = doc.selectFirst("video#play")?.attr("poster")
         val scriptContent = doc.selectFirst("script:containsData('video/mp4')")?.data()
         // val mp4linkRegex = Regex("'src': '([^']+)'")
-        val mp4linkRegex = Regex("""{ src: '([^']+)'""")
+        // val mp4linkRegex = Regex("""{ src: '([^']+)'""")
+        val mp4linkRegex = Regex("""\{(?:\s*"src":\s*"([^"]+)",\s*|"sources":\s*\[\s*{[^}]+}\s*\]\s*)+}""")
         val matchResult = mp4linkRegex.find(scriptContent ?: "")
         val mp4link = matchResult?.groupValues?.get(1)
 // doc.selectFirst("script:containsData('sources: [')")?.data()?.let { Regex("'src': '(.*?)'").find(it)?.groupValues?.get(1) }
