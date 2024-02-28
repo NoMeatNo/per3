2fa00cfe9f1c68632c9dd38ad6754b5406c6f8fd
NoMeatNo <160690260+NoMeatNo@users.noreply.github.com>
2024-02-28 18:30:04 -0500

    Update IBommaProvider.kt

--- IBommaProvider/src/main/kotlin/com/likdev256/IBommaProvider.kt
+++ IBommaProvider/src/main/kotlin/com/likdev256/IBommaProvider.kt
@@ -77,13 +77,13 @@ class IBommaProvider : MainAPI() { // all providers must be an instance of MainA
              val episodes = ArrayList<Episode>()
              val seasons = doc.select("div.row:has(.owl-carousel) .owl-carousel")
              seasons.forEach { season ->
-                 val seasonNumber = season.previousElementSibling().select("span:contains(Season)").text().removePrefix("Season").trim().toIntOrNull() ?: 1
-                 season.select(".item").forEachIndexed { index, episodeElement ->
+                 val seasonNumberText = season.previousElementSibling().select("span:contains(Season)").text().removePrefix("Season").trim()
+                 val seasonNumber = seasonNumberText.toIntOrNull() ?: 1
+                 season.select(".item").forEach { episodeElement ->
                      val episodeLink = episodeElement.select("a")
                      val episodeUrl = episodeLink.attr("href")
                      val episodeTitle = episodeLink.select(".figure-caption").text()
                      val episodeIndex = episodeTitle.filter { it.isDigit() }.toIntOrNull()
-
                      episodes.add(
                          Episode(
                              episodeUrl,
@@ -98,35 +98,8 @@ class IBommaProvider : MainAPI() { // all providers must be an instance of MainA
             val distinctEpisodes = episodes.distinct().sortedBy { it.episode }
             newTvSeriesLoadResponse(title, url, TvType.TvSeries, distinctEpisodes) {
                 this.posterUrl = poster
-       //         this.plot = synopsis
-       //         this.tags = tags
-       //         this.actors = actors
-       //         this.rating = rating
+  
             }
-
-             
-//             val episodes = seasonNumbers.flatMap { seasonNumber ->
-//                document.select(".owl-carousel.season_$seasonNumber .item").mapNotNull { item ->
-//                    val seasonName = "Season $seasonNumber"
-//                    val figcaption = item.select(".figure figcaption").text().trim()
-//                    val episode = figcaption.filter { it.isDigit() }.toIntOrNull()
-//                    val name = if (episode != null) {
-//                        "${figcaption} - $seasonName"
-//                    } else {
-//                        figcaption
-//                    }
-//                    val season = document.select(".movie-heading span").text().trim().removePrefix("Season ").toIntOrNull() ?: 1
-//                    val href = fixUrlNull(item.select("a").attr("href"))
-//                    if (href != null) {
-//                        Episode(data = href, name = name, season = season, episode = episode)
-//                    } else {
-//                        null
-//                    }
-//               }
-//            }
-//            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
-//                this.posterUrl = poster
-//            }
 
         } else {
             return newMovieLoadResponse(title, url, TvType.Movie, url) {
