bf817fce6f4217827149a642a708f19cf1d1c84a
NoMeatNo <160690260+NoMeatNo@users.noreply.github.com>
2024-03-02 13:08:32 -0500

    Update IBommaProvider.kt

--- IBommaProvider/src/main/kotlin/com/likdev256/IBommaProvider.kt
+++ IBommaProvider/src/main/kotlin/com/likdev256/IBommaProvider.kt
@@ -68,7 +68,7 @@ class IBommaProvider : MainAPI() { // all providers must be an instance of MainA
         return resultFarsi.sortedBy { -FuzzySearch.partialRatio(it.name.replace("(\\()+(.*)+(\\))".toRegex(), "").lowercase(), query.lowercase()) }
     }
 
-override suspend fun load(url: String): LoadResponse? {
+    override suspend fun load(url: String): LoadResponse? {
     val document = app.get(url).document
     val title = document.selectFirst("div.col-sm-9 p.m-t-10 strong")?.text()?.trim() ?: return null
     val poster = fixUrlNull(document.selectFirst("video#play")?.attr("poster"))
@@ -85,7 +85,9 @@ override suspend fun load(url: String): LoadResponse? {
             val seasonNumber = seasonNumberText.toIntOrNull()
 
             // Extract episodes within the season
-            val seasonEpisodes = season.select(".owl-carousel.season_${seasonNumberElement?.ownText()} .item")
+            val seasonClass = season.select(".owl-carousel").attr("class").split(" ").find { it.startsWith("season_") }
+            val seasonEpisodes = season.select(".owl-carousel.$seasonClass .item")
+
             seasonEpisodes.forEach { item ->
                 // Extract episode details
                 val figcaption = item.select(".figure-caption").text().trim()
@@ -113,7 +115,7 @@ override suspend fun load(url: String): LoadResponse? {
         }
     }
 }
-
+    
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
