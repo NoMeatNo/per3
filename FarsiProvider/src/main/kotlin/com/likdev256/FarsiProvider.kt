7ff7dfa316bc104ef0c957d7a8d8f3b079b698de
NoMeatNo <160690260+NoMeatNo@users.noreply.github.com>
2024-10-01 09:35:07 -0400

    Update FarsiProvider.kt

--- FarsiProvider/src/main/kotlin/com/likdev256/FarsiProvider.kt
+++ FarsiProvider/src/main/kotlin/com/likdev256/FarsiProvider.kt
@@ -133,7 +133,8 @@ override suspend fun load(url: String): LoadResponse? {
         // Adjust the selectors for movies
         val title = document.selectFirst("div#info h2")?.text()?.trim() ?: return null
 //        val poster = fixUrlNull(document.selectFirst("div#dt_galery .g-item img")?.attr("data-src"))
-        val poster = fixUrlNull(document.selectFirst("#fakeplayer > a > div > img")?.attr("src"))
+//        val poster = fixUrlNull(document.selectFirst("#fakeplayer > a > div > img")?.attr("src"))
+        val poster = fixUrlNull(document.selectFirst("#fakeplayer .playbox img.cover")?.attr("src"))
 //       val poster = fixUrlNull(document.selectFirst("div#fakeplayer img.cover")?.attr("data-src"))
         val plot = document.selectFirst("div#info div.wp-content p")?.text()?.trim()
 
