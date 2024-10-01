c1204ce8a4d5141685a035677bb3a9be1cf7b1de
NoMeatNo <160690260+NoMeatNo@users.noreply.github.com>
2024-10-01 17:23:37 -0400

    Update FarsiProvider.kt

--- FarsiProvider/src/main/kotlin/com/likdev256/FarsiProvider.kt
+++ FarsiProvider/src/main/kotlin/com/likdev256/FarsiProvider.kt
@@ -132,11 +132,8 @@ override suspend fun load(url: String): LoadResponse? {
     } else if (isEpisode) {
         // Adjust the selectors for movies
         val title = document.selectFirst("div#info h2")?.text()?.trim() ?: return null
-        val poster = fixUrlNull(document.selectFirst("div#dt_galery .g-item img")?.attr("data-src"))
-    // w   val poster = fixUrlNull(document.selectFirst("#fakeplayer .playbox img.cover")?.attr("src"))
+        val poster = fixUrlNull(document.selectFirst("#fakeplayer .playbox img.cover")?.attr("src"))
    // w2     val poster = fixUrlNull(document.selectFirst("#dt_galery .g-item img")?.attr("src"))
-   // w3    val poster = fixUrlNull(document.selectFirst("#dt_galery div.owl-wrapper-outer div div div a img")?.attr("src"))
-//       val poster = fixUrlNull(document.selectFirst("div#fakeplayer img.cover")?.attr("data-src"))
         val plot = document.selectFirst("div#info div.wp-content p")?.text()?.trim()
 
         newMovieLoadResponse(title, url, TvType.Movie, url) {
