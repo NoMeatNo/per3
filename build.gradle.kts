8a305efe7504242da7fb6567422fc86852c62eb4
NineStar <boof@NineStars.local>
2025-03-01 07:14:04 -0500

    workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes
-        apk("com.lagradost:cloudstream3:3.3.4")
+        apk("com.lagradost:cloudstream3:4.5.1")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
