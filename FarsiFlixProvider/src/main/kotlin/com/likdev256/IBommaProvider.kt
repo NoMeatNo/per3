ca7de82e25589650df0360b9c4e6a17f1da42389
NineStar <boof@NineStars.local>
2025-03-01 17:40:34 -0500

    workflow

--- FarsiFlixProvider/src/main/kotlin/com/likdev256/IBommaProvider.kt
+++ FarsiFlixProvider/src/main/kotlin/com/likdev256/IBommaProvider.kt
@@ -63,9 +63,10 @@ private fun Element.toLiveTvSearchResult(): LiveSearchResponse? {
     return newLiveSearchResponse(
         name = this.selectFirst("figcaption.figure-caption")?.text() ?: return null,
         url = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null,
-        apiName = this@FarsiFlixProvider.name,  // ✅ Pass `apiName` here
-        type = TvType.Live
     ) {
+        // Set properties in the lambda block
+        this.apiName = this@FarsiFlixProvider.name
+        this.type = TvType.Live
         this.posterUrl = fixUrlNull(this@toLiveTvSearchResult.select("img").attr("data-src"))
     }
 }
